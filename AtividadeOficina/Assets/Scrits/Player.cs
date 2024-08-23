61700d2906cf1d3c5e78688571c63695c425e21a
juliabx <146197172+juliabx@users.noreply.github.com>
2024-08-23 10:30:27 -0300

    Video6, 6:33

--- AtividadeOficina/Assets/Scrits/Player.cs
+++ AtividadeOficina/Assets/Scrits/Player.cs
@@ -8,11 +8,16 @@ public class Player : MonoBehaviour
     public float Speed;
     public float JumpForce;
     private Rigidbody2D rig;
+    private Animator anim;
+
+    public bool isJumping;
+    public bool doubleJump;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,7 +36,33 @@ public class Player : MonoBehaviour
     void Jump(){
         if(Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
+    
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isJumping = true;
     }
 }
